9fd5e9e32bcef0a3ff92bda687674981995f5099
juniorsmars <juniorsmars@gmail.com>
2024-11-20 21:53:43 -0300

    Gerador de Tabuada
    Atividade de Gerador de Tabuada

---
+++ src/Tabuada.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número (1 a 10) para ver a Tabuada: ");
+        int numero = scanner.nextInt();
+            System.out.println("Tabuada do " + numero + ":");
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(numero + " x " + i + " = " + (numero * i));
+            }
+        scanner.close();
+    }
+}
