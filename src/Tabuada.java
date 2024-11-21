35f591c50c5f2ee75f8aaf8cc7629ee4a203d4cb
emir-junior <emir.junior@rede.ulbra.br>
2024-11-20 22:03:30 -0300

    Update Tabuada.java

--- src/Tabuada.java
+++ src/Tabuada.java
@@ -4,7 +4,7 @@ public class Tabuada {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Digite um número (1 a 10) para ver a Tabuada: ");
+        System.out.print("Digite um número para ver a Tabuada: ");
         int numero = scanner.nextInt();
             System.out.println("Tabuada do " + numero + ":");
             for (int i = 1; i <= 10; i++) {
